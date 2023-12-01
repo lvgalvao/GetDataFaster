9fd4feaf44a8a3fb283d1bd3edc7a5360a227ce1
Luciano Filho <lvgalvaofilho@gmail.com>
2023-12-01 03:37:33 -0300

    update

--- src/script_consulta.py
+++ src/script_consulta.py
@@ -1,6 +1,7 @@
 # script principal
 from consulta import Consulta
 from loguru import logger
+import time
 import os
 env = os.getenv('ENV')
 
@@ -18,12 +19,13 @@ consulta = Consulta()
 # logger.info(f"Total de funcionários: {count}")
 
 
-# Consulta 1 - Funcionários com mais de 30 anos (LIMIT 10)
-results = consulta.execute_query("SELECT name, salary FROM employees WHERE age > 20 LIMIT 10", explain=True)
+# Consulta 1
+results = consulta.execute_query("SELECT name, salary FROM employees WHERE age > 30", explain=False)
 # Consulta 2
-results = consulta.execute_query("SELECT name, salary FROM employees WHERE age > 20", explain=True)
-# Consulta 3
-results = consulta.execute_query("SELECT * FROM employees WHERE age > 20", explain=True)
+results = consulta.execute_query("SELECT * FROM employees WHERE age > 30", explain=False)
 
+time.sleep(5)
 
+# Consulta 3 - Funcionários com mais de 30 anos (LIMIT 10)
+results = consulta.execute_query("SELECT name, salary FROM employees WHERE age > 30 LIMIT 10", explain=False)
 
